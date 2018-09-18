89c15529e6d40860a34f9b4ef406ee516e14cb01
Tornz <cantornaeric2024@gmail.com>
2018-09-18 15:07:19 +0800

    fpa phasing

--- src/app/layout/phasing/phasing-routing.module.ts
+++ src/app/layout/phasing/phasing-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhasingComponent } from './phasing.component';
+import { FPAComponent } from './fpa/fpa.component';
+import { BackLogsComponent } from './fpa/backlogs/backlogs.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: PhasingComponent
-    }
+        component: PhasingComponent,
+    },
+    { path: 'fpa', component: FPAComponent },
+    { path: 'backlogs', component: BackLogsComponent }
+   
 ];
 
 @NgModule({
--- src/app/layout/phasing/phasing.component.ts
+++ src/app/layout/phasing/phasing.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-phasing',
     templateUrl: './phasing.component.html',
-    styleUrls: ['./phasing.component.scss']
+    styleUrls: ['./phasing.component.scss'],
+    animations: [routerTransition()]
 })
 export class PhasingComponent implements OnInit {
-    constructor() {}
+    constructor(private router: Router) {}
 
-    ngOnInit() {}
+    ngOnInit() { }
+    onPhaseLoad() {
+        this.router.navigate(['/phasing/fpa']);
+    }
 }
